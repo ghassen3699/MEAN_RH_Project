b64cb8a25cddc8ec8b66f2370b9813aa43596ed3
Ghassen_khammessi <ghassenkhammessi123@icloud.com>
2024-05-26 18:35:31 +0100

    add login router

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,6 +1,7 @@
 const User = require('../models/userModel') ;
 const { validationResult } = require('express-validator') ;
 const bcrypt = require('bcrypt') ;
+const jwt = require('jsonwebtoken') ;
 
 const registerUser = async (req, res) => {
     try {
@@ -45,6 +46,60 @@ const registerUser = async (req, res) => {
     }
 }
 
+
+const generateAccessToken = async( user ) => {
+    const token = await jwt.sign(user, "Tekup", {expiresIn: "2h"}) ;
+    return token ;
+}
+
+const loginUser = async(req, res) => {
+    try {
+        const errors = validationResult(req) ;
+        if(!errors.isEmpty()){
+            return res.status(200).json({
+                success: false,
+                msg: 'Errors',
+                errors: errors.array()
+            })
+        }
+
+        const {email , password} = req.body ;
+        const userData = await User.findOne({ email }) ;
+        if (!userData){
+            return res.status(400).json({
+                success: false,
+                msg: "Email & password is incorrect"
+            }) ;
+        }
+
+        const isPasswordMatch = await bcrypt.compare(password, userData.password) ;
+        if (!isPasswordMatch){
+            return res.status(400).json({
+                success: false,
+                msg: "Email & password is incorrect"
+            }) ;
+        }
+
+        const accessToken = await generateAccessToken({ user: userData }) ;
+
+        return res.status(200).json({
+            success: true,
+            msg: 'Login Successfully',
+            accessToken: accessToken,
+            tokenType:'Bearer',
+            data: userData
+        });
+
+
+    } catch (error) {
+        return res.status(400).json({
+            success: false,
+            msg: error.message
+        }) ;
+    }
+}
+
 module.exports = {
-    registerUser
+    registerUser,
+    loginUser
 }
--- backend/routes/authRoute.js
+++ backend/routes/authRoute.js
@@ -2,8 +2,10 @@ const express = require('express') ;
 const router = express() ;
 const authController = require('../controllers/authController') ;
 const {registerValidator} = require('../helpers/validator') ;
+const {loginValidator} = require('../helpers/validator') ;
 
-router.post('/register', registerValidator, authController.registerUser)
+router.post('/register', registerValidator, authController.registerUser) ;
+router.post('/login', loginValidator, authController.loginUser) ;
 
 
 
